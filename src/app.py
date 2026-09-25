3826b71a20eaabf5415af745b5945a77b8715b6c
NguyenGiaHuy1608 <nguyengiahuy9a2qt@gmail.com>
2026-09-25 15:36:51 +0700

    thay doi noi dung va vi tri

--- src/app.py
+++ src/app.py
@@ -1,1 +1,12 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "CDTN1 - Hệ thống tiếp nhận và phân loại yêu cầu bảo hành"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
